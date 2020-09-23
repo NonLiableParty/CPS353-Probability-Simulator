fc905323e83f165730273899ce8f28437bb7aa40
Brian Prais <brianandrew.prais@yahoo.com>
2020-09-23 18:00:56 -0400

    BASELINE UPLOAD
    Starting point for our project, all assets we create go under content, and all our raw code goes to the source folder. 
    To start developing, just create a new Basic C++ template, with scalable 2d Graphics and no starter content, and boom, empty playground

---
+++ Source/ProbabilitySimulator/ProbabilitySimulatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProbabilitySimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROBABILITYSIMULATOR_API AProbabilitySimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
