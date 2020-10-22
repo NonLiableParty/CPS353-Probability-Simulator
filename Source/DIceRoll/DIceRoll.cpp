ae00d8818d9e80615c38fa9005754b3c401b6b44
binlinchen <binlin1331@gmai.com>
2020-10-21 22:49:25 -0400

    remove files

--- Source/DIceRoll/DIceRoll.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DIceRoll.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DIceRoll, "DIceRoll" );
